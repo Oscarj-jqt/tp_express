bcf150bc600de94953696610dc2e9aa68556e717
Oscarj-jqt <o_jacquet2@hetic.eu>
2024-05-22 16:41:42 +0200

    lastest modifications

--- server/router.js
+++ server/router.js
@@ -12,9 +12,9 @@ app.set('view engine', 'ejs');
 //Création des 3 routes
 
 //Route test page d'accueil
-router.get('/', (req, res) => {
-    res.send('Hello World !');
-})
+// router.get('/', (req, res) => {
+//     res.send('Hello World !');
+// })
 
 // Première route 
 
@@ -23,29 +23,43 @@ const cours = [
     {
         numeroducours: 1,
         titre: "Mathématiques appliquées",
-        descriptif: "Application du savoir mathématique aux autres domaine numérique, statistiques...",
+        description: "Application du savoir mathématique aux autres domaine numérique, statistiques...",
         enseignants: ['Laurent Ménard', 'Lucas Martin','Inès Roy','Enzo Lemoine']
     },
     {
         numeroducours: 2,
         titre: "Electromagnétisme",
-        descriptif: "Bases de l'électromagnétisme, des calculs du champs statiques à la propagation des ondes électromagnétiques",
+        description: "Bases de l'électromagnétisme, des calculs du champs statiques à la propagation des ondes électromagnétiques",
         enseignants: ['Jade Caron', 'Antoine Marchand','Victor Blanchard','Sarah Garnier']
     },
     {
         numeroducours: 3,
         titre: "Mécanique physique",
-        descriptif: "Introduction au concepts de mécanique des fluides, matériaux et systèmes dynamiques",
+        description: "Introduction au concepts de mécanique des fluides, matériaux et systèmes dynamiques",
         enseignants: ['Maxime Roux', 'Sophie Petit','Julie Simon','Théo Richard']
     }
 ]
 
-router.get('/cours/:numeroducours/descr', (req, res) => {
-    const coursNumero = req.params.numeroducours;
-    res.send(`Vous avez demandé le cours numéro ${numeroducours}.`);
+// app.get('/cours/:numeroducours/descr', (req, res) => {
+//     const coursNumero = req.params.numeroducours;
+//     res.send(`Vous avez demandé le cours numéro ${numeroducours}.`);
+// });
+
+app.get('/cours/:numeroducours/descr', (req, res) => {
+    const coursNumero = parseInt(req.params.numeroducours, 10);
+    const enseignement = cours.find(c => c.numeroducours === coursNumero);
+    // S'il parvient à récupérer le cours, il renvoie :
+    if (enseignement) {
+        res.render('cours', {
+            titre: enseignement.titre,
+            description: enseignement.description,
+            enseignants: enseignement.enseignants,
+        });
+        // En cas d'échec
+    } else {
+        res.status(404).send('Cours introuvable');
+    }
 });
-
-
 
 app.use('/', router);
 
